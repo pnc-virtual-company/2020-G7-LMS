75066d1b0a71c8532162f5a168633d28b1ed9a00
senghorkhen <senghorkhen96@gmail.com>
2020-07-10 13:29:20 +0700

    #1 menu

--- app/Views/layouts/menu.php
+++ app/Views/layouts/menu.php
@@ -1,4 +1,3 @@
-
 <nav class="navbar navbar-expand-md bg-dark text-white navbar-dark">
     <img class="navbar-brand" src="images/lms_app.png" alt="logo" width= "40">Leave Management System
   <!-- Toggler/collapsibe Button -->
@@ -18,10 +17,49 @@
         <li class="dropdown ">
          <a href="#" class="dropdown-toggle text-uppercase text-white nav-link " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" >Ronnan</a>
             <div class="dropdown-menu">
-                <a class="dropdown-item" href="#">Profile</a>
+                <a class="dropdown-item" href="#myModal" role="button"data-toggle="modal" >Profile</a>
                 <a class="dropdown-item" href="#">Log out</a>
             </div>
         </li>
     </ul>
   </div>
 </nav>
+
+<div class="bs-example">
+    <!-- Modal HTML -->
+    <div id="myModal" class="modal fade" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                  <h2>My information</h2>
+                </div>
+                <div class="modal-body">
+                <div class="row">
+
+              <div class="col-6">
+                <strong><p>Frist name</p></strong>
+                <strong><p>Last name</p></strong>
+                <strong><p>Departament</p></strong>
+                <strong><p>Position</p></strong>
+                <strong><p>Start date</p></strong>
+              </div>
+          <div class="col-5">
+          <p>Ronan</p>
+                    <p>OGOR</p>
+                    <p>Training and education</p>
+                    <p>WEP Coordinator</p>
+                    <p>25/11/2019</p>
+          </div>
+              
+          </div>
+          <div id="popup1" class="overlay">
+            <div class="popup" >
+              <a class="close text-warning" href="#popup1" data-dismiss = "modal">Close</a>
+            </div>
+           </div>
+        </div> 
+      </div>
+    </div>
+  </div>
+</div>
+</body>
