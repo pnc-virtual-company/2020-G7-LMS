ba16f6db0556b426a5d405132e647253eb963753
senghorkhen <senghorkhen96@gmail.com>
2020-07-10 12:37:01 +0700

    #7 update department

--- app/Views/departments/department.php
+++ app/Views/departments/department.php
@@ -75,4 +75,32 @@
   </div>
   <!-- =================================END MODEL CREATE==================================================== -->
 
+   <!-- ========================================START Model UPDATE================================================ -->
+	<!-- The Modal -->
+	<div class="modal fade" id="updateDepartment">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Edit Department</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body text-right">
+			<form  action="/" method="post">
+				<div class="form-group">
+					<input type="text" class="form-control" value="Edit Department">
+				</div>
+			<a data-dismiss="modal" class="closeModal">DISCARD</a>
+		 	 &nbsp;
+		  <input type="submit" value="UPDATE" class="createBtn text-warning">
+        </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <!-- =================================END MODEL UPDATE==================================================== -->
+
 <?= $this->endSection() ?>
