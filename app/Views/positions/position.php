26604bf30ecdd60e24f5c04dde3c7c9c13549a6b
senghorkhen <senghorkhen96@gmail.com>
2020-07-10 12:59:30 +0700

    #9 update position

--- app/Views/positions/position.php
+++ app/Views/positions/position.php
@@ -73,4 +73,31 @@
   </div>
   <!-- =================================END MODEL CREATE==================================================== -->
 
+ <!-- ========================================START Model UPDATE================================================ -->
+	<!-- The Modal -->
+	<div class="modal fade" id="updatePosition">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Edit Position</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body text-right">
+			<form  action="/" method="post">
+				<div class="form-group">
+					<input type="text" class="form-control" value="Edit Position">
+				</div>
+			<a data-dismiss="modal" class="closeModal">DISCARD</a>
+		 	 &nbsp;
+		  <input type="submit" value="UPDATE" class="createBtn text-warning">
+        </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <!-- =================================END MODEL UPDATE==================================================== -->
 <?= $this->endSection() ?>
