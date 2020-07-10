6712e03243f76f330713fe76e9af7425d222c083
chanthoeurntuon20 <chanthoeurntuon20@gmail.com>
2020-07-10 09:25:32 +0700

    #5 add logo and text on menu

--- app/Views/layouts/menu.php
+++ app/Views/layouts/menu.php
@@ -1,27 +1,27 @@
 
-<nav class="navbar navbar-expand-md bg-img navbar-dark">
+<nav class="navbar navbar-expand-md bg-dark text-white navbar-dark">
+    <img class="navbar-brand" src="images/lms_app.png" alt="logo" width= "40">Leave Management System
   <!-- Toggler/collapsibe Button -->
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
+    <span class="navbar-toggler-icon">
+    <img class="rounded mx-auto d-block" src="images/lms_app.png" alt="" width=150px height=150px>
+    </span>
   </button>
-
   <!-- Navbar links -->
   <div class="collapse navbar-collapse" id="collapsibleNavbar">
     <ul class="nav navbar-nav ml-auto">
-         <a class="nav-link mt-2" href="#">Your leaves</a>
-         <a class="nav-link mt-2" href="#">Leaves</a>
-         <a class="nav-link mt-2" href="#">Employees</a>
-         <a class="nav-link mt-2" href="#">Positions</a>
-         <a class="nav-link mt-2" href="#">Departments</a>
-         <a class="nav-link menu" href="#">
-            <li class="dropdown">
+         <a class="nav-link " href="#">Your leaves</a>
+         <a class="nav-link " href="leaves">Leaves</a>
+         <a class="nav-link " href="#">Employees</a>
+         <a class="nav-link " href="#">Positions</a>
+         <a class="nav-link " href="#">Departments</a>
+        <li class="dropdown ">
          <a href="#" class="dropdown-toggle text-uppercase text-white nav-link " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" >Ronnan</a>
             <div class="dropdown-menu">
                 <a class="dropdown-item" href="#">Profile</a>
                 <a class="dropdown-item" href="#">Log out</a>
-              </div>
-          </li>
-         </a>
+            </div>
+        </li>
     </ul>
   </div>
 </nav>
--- app/Views/leaves/leave.php
+++ app/Views/leaves/leave.php
@@ -1,8 +1,10 @@
+
 <!-- Load icon library -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <?= $this->extend('layouts/main') ?>
-<?= $this->section('content') ?><br><br>
-<div class="container mt-4">
+<?= $this->section('content') ?>
+<?= $this->include('layouts/menu') ?>
+<div class="container mt-5">
     <div class="input-group mb-3">
         <input type="text" id="search" class="form-control" placeholder="Search">
         <div class="input-group-append">
