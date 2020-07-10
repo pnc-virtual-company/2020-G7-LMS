adde6ab50f07f0bb13babfbd3d49321f201f238e
senghorkhen <senghorkhen96@gmail.com>
2020-07-10 12:35:48 +0700

    #7 create department

--- app/Views/departments/department.php
+++ app/Views/departments/department.php
@@ -47,4 +47,32 @@
         </div>
     </div>
 
+<!-- ===========================================START MODEL CREATE ================================== -->
+	<!-- The Modal -->
+	<div class="modal fade" id="createDepartment">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Create Department</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body text-right">
+			<form  action="/" method="post">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Department name">
+				</div>
+			<a data-dismiss="modal" class="closeModal">DISCARD</a>
+		 	 &nbsp;
+		  <input type="submit" value="CREATE" class="createBtn text-warning">
+        </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <!-- =================================END MODEL CREATE==================================================== -->
+
 <?= $this->endSection() ?>
