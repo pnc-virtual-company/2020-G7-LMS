6a00f88fd8f0e8b3f8b5a425984ef0564fd2b88b
chanthoeurntuon20 <chanthoeurntuon20@gmail.com>
2020-07-12 19:33:48 +0700

    #4 create logout

---
+++ app/Controllers/Logout.php
@@ -0,0 +1,11 @@
+<?php namespace App\Controllers;
+
+class Logout extends BaseController
+{
+	public function logout()
+	{
+        session()->destroy();
+		return redirect()->to('/');
+	}
+	//--------------------------------------------------------------------
+}
