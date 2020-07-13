1f6dc5b63513eb4d8ee6d280ced5798d9c5b0fc8
senghorkhen <senghorkhen96@gmail.com>
2020-07-13 11:05:37 +0700

    #7 change style department view

--- app/Views/departments/department.php
+++ app/Views/departments/department.php
@@ -6,68 +6,54 @@
         <div class="row">
             <div class="col-3"></div>
             <div class="col-6">
-                <div class="card-body">
-                    <form action="">
                     <div class="input-group mb-3">
                       <input type="text" class="form-control" id="search" placeholder="Search">
                     </div>
                     <br>
-                    <ul class="text-right">
+                    <div class="text-right">
                         <a href="" class="btn btn-primary btn-sm text-white font-weight-bolder" data-toggle="modal" data-target="#createDepartment">
                         <i class="material-icons float-left" data-toggle="tooltip" title="Create Department!" data-placement="left">add</i>&nbsp;Create
                         </a>
                     </div>
-                    <table class="table table-borderless table-hover">
-                      <div class="col-12">
-                      <div class="list-group">
-                        <div class="list-group-item list-group-item-action active">
-                            <span>Departments</span>
-                        </div>
-                        <div class="list-group-item list-group-item-action">                        
-                          <div class="row">
-                            <i class="material-icons">people</i> 
-                              <span>Training and education team</span>
-                              <i class="icon1-blog">
-                            <a href="" data-toggle="modal" data-target="#updateDepartment"><i class="material-icons text-info" data-toggle="tooltip" title="Edit Department!"  data-placement="left">edit</i></a>
+                    <br>
+                    <table class="table table-hover">
+                <thead class="bg-primary text-white">
+                    <tr>
+                        <th>Department</th>
+                        <th class="text-right">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><i class="material-icons float-left">people</i> &nbsp;&nbsp; Training and education team</td>
+                        <td class="text-right">
+                        <a href="" data-toggle="modal" data-target="#updateDepartment"><i class="material-icons text-info" data-toggle="tooltip" title="Edit Department!"  data-placement="left">edit</i></a>
                             <a href="" data-toggle="tooltip" title="Delete Department!" data-placement="right" class="delete"><i class="material-icons text-danger" onclick="return confirm('Are you sure you want to remove the selected departments?');">delete</i></a>
-                            </i>
-                          </div>  
-                        </div>
-                        <div class="list-group-item list-group-item-action">
-                        <div class="row">
-                            <i class="material-icons">people</i> 
-                              <span>External relation team</span>
-                              <i class="icon1-blog">
-                            <a href="" data-toggle="modal" data-target="#updateDepartment"><i class="material-icons text-info" data-toggle="tooltip" title="Edit Department!"  data-placement="left">edit</i></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><i class="material-icons float-left">people</i> &nbsp;&nbsp; Education </td>
+                        <td class="text-right">
+                        <a href="" data-toggle="modal" data-target="#updateDepartment"><i class="material-icons text-info" data-toggle="tooltip" title="Edit Department!"  data-placement="left">edit</i></a>
                             <a href="" data-toggle="tooltip" title="Delete Department!" data-placement="right" class="delete"><i class="material-icons text-danger" onclick="return confirm('Are you sure you want to remove the selected departments?');">delete</i></a>
-                            </i>
-                          </div>  
-                        </div>
-                        <div class="list-group-item list-group-item-action">
-                        <div class="row">
-                            <i class="material-icons">people</i> 
-                              <span>Admin and finance team </span>
-                              <i class="icon1-blog">
-                            <a href="" data-toggle="modal" data-target="#updateDepartment"><i class="material-icons text-info" data-toggle="tooltip" title="Edit Department!"  data-placement="left">edit</i></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><i class="material-icons float-left">people</i> &nbsp;&nbsp; HR </td>
+                        <td class="text-right">
+                        <a href="" data-toggle="modal" data-target="#updateDepartment"><i class="material-icons text-info" data-toggle="tooltip" title="Edit Department!"  data-placement="left">edit</i></a>
                             <a href="" data-toggle="tooltip" title="Delete Department!" data-placement="right" class="delete"><i class="material-icons text-danger" onclick="return confirm('Are you sure you want to remove the selected departments?');">delete</i></a>
-                            </i>
-                          </div> 
-                        </div>
-                        <div class="list-group-item list-group-item-action">
-                        <div class="row">
-                            <i class="material-icons">people</i> 
-                              <span>Selection team</span>
-                              <i class="icon1-blog">
-                            <a href="" data-toggle="modal" data-target="#updateDepartment"><i class="material-icons text-info" data-toggle="tooltip" title="Edit Department!"  data-placement="left">edit</i></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><i class="material-icons float-left">people</i> &nbsp;&nbsp; IT Admin </td>
+                        <td class="text-right">
+                        <a href="" data-toggle="modal" data-target="#updateDepartment"><i class="material-icons text-info" data-toggle="tooltip" title="Edit Department!"  data-placement="left">edit</i></a>
                             <a href="" data-toggle="tooltip" title="Delete Department!" data-placement="right" class="delete"><i class="material-icons text-danger" onclick="return confirm('Are you sure you want to remove the selected departments?');">delete</i></a>
-                            </i>
-                          </div> 
-                        </div>
-                      </ul>
-                      </div>
-				            </table>
-                  </form>
-                </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
             </div>
             <div class="col-3"></div>
         </div>
