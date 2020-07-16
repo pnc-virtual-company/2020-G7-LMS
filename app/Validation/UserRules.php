98d2cbbbb3958ff62d964fbf8aa3740df7382eac
senghorkhen <senghorkhen96@gmail.com>
2020-07-16 16:07:48 +0700

    #13 update: Validation

--- app/Validation/UserRules.php
+++ app/Validation/UserRules.php
@@ -5,7 +5,8 @@ class UserRules{
     public function validateUser(string $str,string $fields, array $data){
         $model = new UserModel();
         $user = $model->where('email',$data['email'])->first();
-        if(!$user){
+        $password = $model->where('password',$data['password'])->first();
+        if(!$user || !$password ){
             return false;
         return password_verify($data['password'],$user['password']);
         }
