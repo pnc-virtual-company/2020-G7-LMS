923cd3d44e4d04aa67c624c34ff5537938049cff
soklyphornweb2020 <sokly.phorn@student.passerellesnumeriques.org>
2020-07-22 17:03:53 +0700

    #15 update new button search on position

--- app/Views/positions/position.php
+++ app/Views/positions/position.php
@@ -5,9 +5,9 @@
     <div class="row">
         <div class="col-3"></div>
         <div class="col-6">
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" id="search" placeholder="Search">
-            </div>
+        <div class="input-group mb-3">
+        <input type="text" class="form-control" id="search" onkeyup="myFunction()" placeholder="Search">
+        </div>
             <br>
             <div class="text-right">
                 <a href="" class="btn btn-primary btn-sm text-white font-weight-bolder" data-toggle="modal"
@@ -18,7 +18,7 @@
                 </a>
             </div>
             <br>
-            <table class="table table-hover ">
+            <table class="table table-hover " id="myTable">
                 <thead class="bg-info text-white">
                     <tr>
                         <th>Positions</th>
