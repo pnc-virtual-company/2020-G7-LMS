68fb2827ea6ef9450b329c79dff6ba8dca9c29a5
chanthoeurntuon20 <chanthoeurntuon20@gmail.com>
2020-07-22 23:29:21 +0700

    #23 edit employee

--- app/Views/employees/index.php
+++ app/Views/employees/index.php
@@ -11,37 +11,44 @@
     </div>
     <!-- button create Employee -->
     <div class="text-right">
-         <a href="" class="btn btn-primary btn-sm text-white font-weight-bolder" data-toggle="modal" data-target="#createEmployee" style="margin-right:65px;">
-			<i class="material-icons float-left" data-toggle="tooltip" title="Add Employee!" data-placement="left">add</i>&nbsp;Create
-		</a> 
+        <a href="" class="btn btn-primary btn-sm text-white font-weight-bolder" data-toggle="modal"
+            data-target="#createEmployee" style="margin-right:65px;">
+            <i class="material-icons float-left" data-toggle="tooltip" title="Add Employee!"
+                data-placement="left">add</i>&nbsp;Create
+        </a>
     </div>
     <!-- Text on Employee -->
-     <h4 class="font-weight-bold ml-2">List  Employee</h4><br>
-     <!-- table Employee -->
-		<table class="table table-borderless table-hover ">
-			<tr>
-                <th class= "hide">ID</th>
-                <th>First name</th>
-                <th>Last name</th>
-                <th>Email</th>
-                <th>Department</th> 
-                <th>Position</th>
-                <th>Start date</th>
-            </tr>
-			<?php foreach ($userData as $user): ?>
-                <tr>
-                    <td class = "hide"><?= $user['id']?></td>
-                    <td><?= $user['firstname']?></td>
-                    <td><?= $user['lastname']?></td>
-                    <td><?= $user['email']?></td>
-                    <td><?= $user['de_name']?></td>
-                    <td><?= $user['po_name'] ?></td>
-                    <td><?= $user['start_date']?></td>
-                <td >
-                    <!-- Icon delete and edit -->
-                    <div class='edit_hover_class row'>
-                        <a href="" data-toggle="modal" data-target="#updateEmployee" class="icon-edit"><i class="material-icons text-info btn_update" data-toggle="tooltip" title="Edit Employee!" data-placement="left" style="margin-right: 12px;">edit</i></a>
-                        <a href="employee/delete/<?= $user['id']?>" data-toggle="modal" data-target="#deleteEmployee<?= $user['id']?>" data-toggle="tooltip" title="Delete Employee!" data-placement="right" class="delete"><i class="material-icons text-danger">delete</i></a>
+    <h4 class="font-weight-bold ml-2">List Employee</h4><br>
+    <!-- table Employee -->
+    <table class="table table-borderless table-hover ">
+        <tr>
+            <th class="hide">ID</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Email</th>
+            <th class="hide">Password</th>
+            <th class="hide">Role</th>
+            <th class="hide">Profile</th>
+            <th>Department</th>
+            <th>Position</th>
+            <th>Start date</th>
+           
+        </tr>
+        <?php foreach ($userData as $user): ?>
+        <tr class='edit_hover_class'>
+            <td class="hide"><?= $user['id']?></td>
+            <td><?= $user['firstname']?></td>
+            <td><?= $user['lastname']?></td>
+            <td><?= $user['email']?></td>
+            <td class = "hide"><?= $user['password']?></td>
+            <td class = "hide"><?= $user['role']?></td>
+            <td class = "hide"><?= $user['profile']?></td>
+            <td><?= $user['de_name']?></td>
+            <td><?= $user['po_name'] ?></td>
+            <td><?= $user['start_date']?></td>
+            <td >  
+                    <button type="button" class="not-btn edit-btn-employee"><i class="material-icons text-info">edit</i></button>
+                    <a href="employee/delete/<?= $user['id']?>" data-toggle="modal" data-target="#deleteEmployee<?= $user['id']?>" data-toggle="tooltip" title="Delete Employee!" data-placement="right" class="delete"><i class="material-icons text-danger">delete</i></a>
                     </div>
                 </td>
             </tr>
@@ -67,186 +74,6 @@
           <?php endforeach; ?>
         </tbody>
       </table>
-      
-<!-- ========================================START Model CREATE================================================ -->
-<!-- The Modal -->
-<div class="modal fade" id="createEmployee">
-    <div class="modal-dialog">
-        <div class="modal-content">
-
-            <!-- Modal Header -->
-            <div class="modal-header">
-                <h4 class="modal-title">Create Employee</h4>
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-            </div>
-            <!-- Modal body -->
-            <div class="modal-body text-right">
-                <form action="employee/add" method="post" enctype="multipart/form-data">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <!-- input First name -->
-                            <div class="form-group pmd-textfield pmd-textfield-floating-label">
-                                <input type="text" class="form-control" placeholder="First name" name="firstname">
-                            </div>
-                            <!-- input Last name -->
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Last name" name="lastname">
-                            </div>
-
-                        </div>
-                        <div class="col-sm-6">
-                            <!-- input Email -->
-                            <div class="form-group pmd-textfield pmd-textfield-floating-label">
-                                <input type="Email" class="form-control" placeholder="Email" name="email">
-                            </div>
-                            <!-- input password -->
-                            <div class="form-group">
-                                <input type="password" class="form-control" placeholder="Password" name="password" >
-                            </div>
-
-                        </div>
-                    </div>
-                    <!-- input Department -->
-                    <div class="form-group">
-                        <select class="form-control" name="department">
-                            <option  value = "" selected disabled>Department</option>
-                            <?php foreach ($departmentData as $department ): ?>
-                                <option value="<?= $department['de_id']?>"> <?= $department['de_name'] ?></option>
-                            <?php endforeach;?>  
-                        </select>
-                    </div>
-                    <!-- Position -->
-                    <div class="form-group">
-                        <select class="form-control" name="position">
-                            <option  value = "" selected disabled>Position</option>
-                            <?php foreach ($positionData as $position ): ?>
-                                <option value="<?= $department['de_id']?>"><?= $position['po_name'] ?></option>
-                            <?php endforeach;?>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <select class="form-control" name="role">
-                            <option selected>Role</option>
-                            <option>Manager</option>
-                            <option>Employee</option>
-                            <option>HR</option>
-                            <option>Admin</option>
-                        </select>
-                    </div>
-                    <!-- input first startdate -->
-                    <div class="form-group">
-                        <input class="form-control datetimepicker" type="date" id = "startdate" data-date-format="DD-YY-MM"
-                        name="startdate" class="form-control" placeholder="start date" required>
-                    </div>
-
-                    <!-- profile -->
-                    <div class="form-group">
-                        <input type="file" class="form-control-file border" name = "profile">
-                    </div>
-
-                    <a data-dismiss="modal" class="closeModal">DISCARD</a>
-                    &nbsp;
-                    <!-- input submit -->
-                    <input type="submit" value="CREATE" class="btn text-primary">
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-  <!-- =================================END MODEL CREATE==================================================== -->
-
-  <!-- ========================================START Model UPDATE================================================ -->
-	<!-- The Modal -->
-<div class="modal fade" id="updateEmployee">
-    <div class="modal-dialog">
-        <div class="modal-content">
-      
-            <!-- Modal Header -->
-            <div class="modal-header">
-                <h4 class="modal-title">Update Empoyee</h4>
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-            </div>
-        
-             <!-- Modal body -->
-            <div class="modal-body text-right">
-                <form  action="/" method="post">
-                <div class="row">
-                    <div class="col-sm-6">
-                            <!-- input First name -->
-                        <div class="form-group pmd-textfield pmd-textfield-floating-label">
-                            <input type="text" class="form-control" placeholder="First name">
-                        </div>
-                            <!-- input Last name -->
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Last name">
-                        </div>
-
-                    </div>
-
-                    <div class="col-sm-6">
-                            <!-- input Email -->
-                        <div class="form-group pmd-textfield pmd-textfield-floating-label">
-                            <input type="Email" class="form-control" placeholder="Email">
-                        </div>
-                            <!-- input password -->
-                        <div class="form-group">
-                            <input type="password" class="form-control" placeholder="Password">
-                        </div>
-
-                    </div>
-                </div> 
-
-                    <!-- input role -->
-                        <div class="form-group">
-                            <select class="form-control" placeholder="Department">
-                                <option selected>Role</option>
-                                <option>Employees</option>
-                                <option>HR Officer</option>
-                                <option>Admin</option>
-                                <option>Manager</option>
-                            </select>
-                        </div>
-                        
-                    <!-- Department -->
-                    <div class="form-group">
-                        <select class="form-control" placeholder="Department">
-                            <option selected>Department</option>
-                            <option>Training/Education</option>
-                            <option>Exteral relation team</option>
-                            <option>Admin and finance team</option>
-                            <option>Selection team</option>
-                        </select>
-                    </div>
-
-                    <!-- Position -->
-                    <div class="form-group">
-                        <select class="form-control" placeholder="Position">
-                            <option selected>Position</option>
-                            <option>IT Admin</option>
-                            <option>WEP Coordinator</option>
-                        </select>
-                    </div>
-                    
-                    <!-- input first startdate -->
-                    <div class="form-group">
-                        <input class="form-control" type="date" data-date=""  data-date-format="DD-YY-MM"
-                        name="startdate"  class="form-control" placeholder="start date" required>
-                    </div>
-                    
-                    <!-- profile -->
-                    <div class="form-group">
-                        <input type="file" class="form-control-file border">
-                    </div>
-
-                    <a data-dismiss="modal" class="closeModal">DISCARD</a>
-                    &nbsp;
-                    <input type="submit" value="UPDATE" class="btn text-info">
-                 
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-  <!-- =================================END MODEL UPDATE==================================================== -->
-<?= $this->endSection() ?>
-
+    <?= $this->include('employees/createEmployeeModal') ?>
+    <?= $this->include('employees/editEmployeeModal') ?> 
+    <?= $this->endSection() ?>
