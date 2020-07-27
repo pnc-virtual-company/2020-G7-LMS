172816ea63165f732ae8dfa87d10591dff02d133
nisa yourm <nisayourm@gmail.com>
2020-07-27 22:45:57 +0700

    #39 update validation encrap employees

--- app/Controllers/Employee.php
+++ app/Controllers/Employee.php
@@ -22,121 +22,132 @@ class Employee extends BaseController
 		];
 		return view('employees/index',$data);
 	}
+
+	
+
+
 	// add employee
-	public function addEmployee(){
-		$data = [];
-		if($this->request->getMethod() == "post"){
-		helper(['form']);
-		$rules = [
-			'firstname'=>'required|min_length[3]|max_length[20]|alpha',
-			'lastname'=>'required|min_length[3]|max_length[20]|alpha',
-			'email'=>'required|valid_email|min_length[6]|max_length[50]',
-			'password'=>'required|min_length[8]|max_length[225]',
-			];		
+public function addEmployee(){
+	$data = [];
+	if($this->request->getMethod() == "post"){
+	helper(['form']);
+	$rules = [
+		'firstname' =>[
+		'rules'=>'required|is_unique[users.firstname]|min_length[3]|max_length[20]',
+		'errors'=>[
+		'required'=>'Last name cannot empty',
+		'is_unique' => 'This employee firstname already exists.',
+	],
+	],
+		'lastname' =>[
+		'rules'=>'required|min_length[3]|max_length[20]',
+		'errors'=>[
+		'required'=>'First name is cannot empty',
+		'is_unique' => 'This employee lastname already exists.',
+	],
+	],
+		'email' =>[
+		'rules'=>'required|min_length[6]|max_length[50]|valid_email',
+		'errors'=>[
+		'required'=>'Email address is cannot empty',
+		'is_unique' => 'Email already exists.'
+	],
+	],
+		'department' =>[
+		// 'rules'=>'required|min_length[3]|max_length[20]',
+	    'errors'=>[
+		'required'=>'Department name is cannot empty'
+	],
+	],
+		'positon' =>[
+		// 'rules'=>'required|min_length[3]|max_length[20]',
+	    'errors'=>[
+		'required'=>'Positon name is cannot empty'
+	],
+	],
+	];
+	
+	if($this->validate($rules)){
+	
+		$firstname = $this->request->getVar('firstname');
+		$lastname = $this->request->getVar('lastname');
+		$email= $this->request->getVar('email');
+		$password = $this->request->getVar('password');
+		$role = $this->request->getVar('role');
+		$department = $this->request->getVar('department');
+		$position = $this->request->getVar('position');
+		$start_date = $this->request->getVar('startdate');
+		$file = $this->request->getFile('profile');
+		$employeeProfile= $file->getRandomName();
 
-		if($this->validate($rules)){
+	$employeeData = array(
+		'firstname'=>$firstname,
+		'lastname'=>$lastname,
+		'email'=>$email,
+		'password'=>$password,
+		'role'=>$role,
+		'profile'=>$employeeProfile,
+		'department_id' => $department,
+		'position_id' => $position,
+		'start_date'=>$start_date
+	);
 
-				$firstname = $this->request->getVar('firstname');
-				$lastname = $this->request->getVar('lastname');
-				$email= $this->request->getVar('email');
-				$password = $this->request->getVar('password');
-				$role = $this->request->getVar('role');
-				$department = $this->request->getVar('department');
-				$position = $this->request->getVar('position');
-				$start_date = $this->request->getVar('startdate');
-				$file = $this->request->getFile('profile');
-				$employeeProfile= $file->getRandomName();
-            // $position = $this->request->getVar('po_name');
-            $data = array(
-				
-				'firstname'=>$firstname,
-				'lastname'=>$lastname,
-				'email'=>$email,
-				'password'=>$password,
-				'role'=>$role,
-				'profile'=>$employeeProfile,
-				'start_date'=>$start_date,
-				'department'=>$department,
-				'position'=>$position
-            );
-            $this->users->insert($data);
-            return redirect()->to('/employee');
-            }else{
-                $data['validation'] = $this->validator;
-                $sessionError = session();
-                $validation = $this->validator;
-                $sessionError->setFlashdata('error', $validation);
-                return redirect()->to('/employee');
-            }
-        }
-    }
-		// delete employee
+	$this->users->registerUser($employeeData);
+		$sessionSuccess = session();
+		$sessionSuccess->setFlashdata('success','Successful insert employee!');
+	}else{
+		$sessionError = session();
+		$validation = $this->validator;
+		$sessionError->setFlashdata('error', $validation);
+	}
+	}
+	return redirect()->to('/employee');
+	// delete employee
+	}
 		public function deleteEmployee($id){
 			$employee = new UserModel();
 			$employee->delete($id);
 			return redirect()->to('/employee');
 		}
+		// update Employee
 		public function updateEmployee(){
-			$employeeData = [];
-				if($this->request->getMethod() == "post"){
+			$data = [];
+			if($this->request->getMethod() == "post"){
 				helper(['form']);
-				//validation form
 				$rules = [
-					'firstname' =>[
-						'rules'=>'required|is_unique[users.firstname]|min_length[3]|max_length[20]',
-						'errors'=>[
-						'required'=>'Last name cannot empty',
-						'is_unique' => 'This employee firstname already exists.',
-						],
-					],
-					'lastname' =>[
-						'rules'=>'required|min_length[3]|max_length[20]',
-						'errors'=>[
-						'required'=>'First name is cannot empty'
-						],
-					],
-					'email' =>[
-						'rules'=>'required|min_length[6]|max_length[50]|valid_email',
-						'errors'=>[
-						'required'=>'Email address is cannot empty',
-						'is_unique' => 'Email already exists.'
-						],
-					]
-				];			
+					'firstname'=>'required|min_length[3]|max_length[20]|alpha',
+					'lastname'=>'required|min_length[3]|max_length[20]|alpha',
+					'email'=>'required|valid_email|min_length[6]|max_length[50]',
+					'password'=>'required|min_length[8]|max_length[225]',
+				];
+
+			 if($this->validate($rules)){
 					$id = $this->request->getVar('user_id');
 					$firstname = $this->request->getVar('firstname');
 					$lastname = $this->request->getVar('lastname');
 					$email= $this->request->getVar('email');
 					$password = $this->request->getVar('password');
 					$role = $this->request->getVar('role');
-					$profile= $this->request->getVar('profile');
 					$department = $this->request->getVar('department');
 					$position = $this->request->getVar('position');
 					$start_date = $this->request->getVar('startdate');
-					
-				if($this->validate($rules) ){
-					$employeeData = array(
-						'firstname'=>$firstname,
-						'lastname'=>$lastname,
-						'email'=>$email,
-						'password'=>$password,
-						'role'=>$role,
-						'profile'=>$profile,
-						'start_date'=>$start_date,
-						'department_id'=>$department,
-						'position_id'=>$position
-				);
-				$this->users->update($id, $employeeData);
-					$sessionSuccess = session();
-					$sessionSuccess->setFlashdata('success','Successful update employee!');
-				}else{
-					$sessionError = session();
-					$validation = $this->validator;
-					$sessionError->setFlashdata('error', $validation);
-				}
-			}
-				return redirect()->to('/employee');
-		}
 
-	
-}
+						$employeeData = array(
+									'firstname'=>$firstname,
+									'lastname'=>$lastname,
+									'email'=>$email,
+								    'department_id'=>$department,
+									'position_id'=>$position
+								);
+							$this->users->update($id, $employeeData);
+							$sessionSuccess = session();
+							$sessionSuccess->setFlashdata('success','Successful update employee!');
+						}else{
+							$sessionError = session();
+							$validation = $this->validator;
+							$sessionError->setFlashdata('error', $validation);
+						}
+					}
+			return redirect()->to('/employee');
+	}
+} 
