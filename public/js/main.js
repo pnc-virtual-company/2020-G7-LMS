948c82b1468b5cdb50591efd204b0629aa8efa31
chanthoeurntuon20 <chanthoeurntuon20@gmail.com>
2020-08-08 14:37:49 +0700

    update update employee in main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,25 @@
 $(document).ready(function () {
-            $('.updateEmployee').on('click', function () {
-                $tr = $(this).closest('tr');
-                var data = $tr.children('td').map(function () {
-                    return $(this).text();
-                }).get();
-                $('#id').val(data[0]);
-                $('#firstname').val(data[1]);
-                $('#lastname').val(data[2]);
-                $('#email').val(data[3]);
-                $('#password').val(data[4]);
-                $('#department').val(data[5]);
-                $('#position').val(data[6]);
-                $('#position').val(data[6]);
-                $('#start_date').val(data[7]);
-            });
+    $(document).ready(function(){
+        $('.edit-btn-employee').on('click',function(){
+            $('#updateEmployee').modal('show');
+
+            $tr = $(this).closest('tr');
+            var data = $tr.children("td").map(function() {
+            return $(this).text();
+            }).get();
+            
+            $('#update_id').val(data[0]);
+            $('#firstname').val(data[1]);
+            $('#lastname').val(data[2]);
+            $('#email').val(data[3]);
+            $('#password').val(data[4]);
+            $('#role').val(data[5]);
+            $('#profile').val(data[6]);
+            $('#startdate').val(data[7]);
+            $('#department_id:selected').val(data[8]);
+            $('#position_id:selected').val(data[9]);
+        });
+    })
             // Update department information modal
             $('.edit-btn-department').on('click', function () {
                 $('#updateDepartment').modal('show');
