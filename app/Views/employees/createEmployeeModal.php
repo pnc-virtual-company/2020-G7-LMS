8bc0b58531d7c9322bf4d5327519bbccec94e60c
nisa yourm <nisayourm@gmail.com>
2020-08-11 10:27:49 +0700

    #24 delete screapt on createEmployeeMoodal

--- app/Views/employees/createEmployeeModal.php
+++ app/Views/employees/createEmployeeModal.php
@@ -99,17 +99,4 @@
     </div>
 </div>
   <!-- =================================END MODEL CREATE==================================================== -->
-  <script>
-  formElem.onsubmit = async (e) => {
-    e.preventDefault();
 
-    let response = await fetch('/article/formdata/post/user', {
-      method: 'POST',
-      body: new FormData(formElem)
-    });
-
-    let result = await response.json();
-
-    alert(result.message);
-  };
-</script>
